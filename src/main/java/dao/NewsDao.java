eef4d4866b21ec3061e4003dfd1f7780d522159f
priscahlimo <priscahlimoh@gmail.com>
2022-03-16 16:45:35 +0300

    add news DAO methods

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,26 @@
 package dao;
 
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+
+//    implement CRUD
+    //create
+
+    void add(News news);
+    void addNewsToDepartment(int deptid, int newsid, int userid);
+
+    //read
+    News findById(int id);
+    List<News> allNews();
+    List<News> allGeneralNews();
+    List<News> allDepartmentalNews();
+
+    //update
+
+    //delete
+    void deleteById(int id);
+    void deleteAll();
 }
