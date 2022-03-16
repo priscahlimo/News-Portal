086d1c0c2a9fc516be140c47e1d19b17909fe86c
priscahlimo <priscahlimoh@gmail.com>
2022-03-16 17:15:12 +0300

    generate user dao implementations

--- src/main/java/dao/UserDaoImplementation.java
+++ src/main/java/dao/UserDaoImplementation.java
@@ -1,4 +1,39 @@
 package dao;
 
-public class UserDaoImplementation {
+import models.News;
+import models.User;
+
+import java.util.List;
+
+public class UserDaoImplementation implements UserDao {
+
+    @Override
+    public void add(User user) {
+
+    }
+
+    @Override
+    public User findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<User> allUsers() {
+        return null;
+    }
+
+    @Override
+    public List<News> myNews(int userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
