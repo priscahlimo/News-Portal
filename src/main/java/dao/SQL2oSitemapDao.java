6e106666d838657e744c52a66f7e875391b8fa6b
priscahlimo <priscahlimoh@gmail.com>
2022-03-16 17:32:23 +0300

    create sitemap models

--- src/main/java/dao/SQL2oSitemapDao.java
+++ src/main/java/dao/SQL2oSitemapDao.java
@@ -1,4 +1,25 @@
 package dao;
 
+import models.Sitemap;
+import models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
 public class SQL2oSitemapDao {
+    public SQL2oSitemapDao() {
+    }
+
+
+    public List<Sitemap> allPaths() {
+        String sql = "SELECT * from sitemap;";
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .executeAndFetch(Sitemap.class);
+        }
+    }
+
+
+
 }
--- src/main/java/models/Sitemap.java
+++ src/main/java/models/Sitemap.java
@@ -1,4 +1,24 @@
 package models;
 
 public class Sitemap {
+    private String descr;
+    private String path;
+    private String id;
+
+    public Sitemap(String descr, String path) {
+        this.descr = descr;
+        this.path = path;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
