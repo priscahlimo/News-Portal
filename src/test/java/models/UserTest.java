76a8c8a3db15df36313f6f17da8f42c3f56fd632
priscahlimo <priscahlimoh@gmail.com>
2022-03-16 16:17:29 +0300

    implement user test

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -1,7 +1,36 @@
 package models;
 
-import static org.junit.Assert.*;
+import dao.DatabaseRule;
+import org.junit.Rule;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@SuppressWarnings("ConstantConditions")
 public class UserTest {
+
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    private User newUser(){
+        return new User("The Star","Manager","Author","Media");
+    }
+
+    @Test
+    public void initializeCorrectly(){
+        User user = newUser();
+        assertTrue(user instanceof User);
+    }
+
+    @Test
+    public void getFunctionsWorkWell(){
+        User user = newUser();
+        assertEquals(user.getName(),newUser().getName());
+        assertEquals(user.getPosition(),newUser().getPosition());
+        assertEquals(user.getRole(),newUser().getRole());
+        assertEquals(user.getDepartment(),newUser().getDepartment());
+    }
+
 
 }
