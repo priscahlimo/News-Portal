2a1b6fddf091ff263ace514028d811f99f041ebe
priscahlimo <priscahlimoh@gmail.com>
2022-03-16 17:37:01 +0300

    update Api exception class

--- src/main/java/ApiException.java
+++ src/main/java/ApiException.java
@@ -1,2 +1,12 @@
-public class ApiException {
+public class ApiException extends RuntimeException {
+    private final int StatusCode;
+
+    public int getStatusCode() {
+        return StatusCode;
+    }
+
+    public ApiException(String message, int statusCode) {
+        super(message);
+        StatusCode = statusCode;
+    }
 }
