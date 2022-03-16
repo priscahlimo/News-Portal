903fdab42dc72f7c029cc1eab82e137a3028b856
priscahlimo <priscahlimoh@gmail.com>
2022-03-16 15:52:22 +0300

    add department properties and respective tests

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,17 +1,32 @@
 package models;
 
-import org.junit.After;
-import org.junit.Before;
+import dao.DatabaseRule;
+import org.junit.Rule;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+@SuppressWarnings("ConstantConditions")
 public class DepartmentTest {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
 
-    @Before
-    public void setUp() throws Exception {
+    private Department newDept(){
+        return new Department("Sales","We Strive for the best");
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void initializeCorrectly(){
+        Department department = newDept();
+        assertTrue(department instanceof Department);
     }
+
+    @Test
+    public void getMethodsWorkCorrectly(){
+        Department department = newDept();
+        assertEquals("Sales",department.getName());
+        assertEquals("We Strive for the best",department.getDescription());
+    }
+
 }
