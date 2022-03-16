cf676aa88fbb60013c4241ebe1cb05558cc75824
priscahlimo <priscahlimoh@gmail.com>
2022-03-16 16:11:45 +0300

    add news test and its implementation

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,17 +1,48 @@
 package models;
 
-import org.junit.After;
-import org.junit.Before;
+import dao.DatabaseRule;
+import org.junit.Rule;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class NewsTest {
 
-    @Before
-    public void setUp() throws Exception {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    private News simpleNews(){
+        return new News( "Simple News", "Currently we have nothing to report");
     }
 
-    @After
-    public void tearDown() throws Exception {
+    private News altNews(){
+        return new News("Food Prices","Kenyans demand food prices to be lowered");
     }
+
+    @Test
+    public void initializeCorrectly(){
+        News news = altNews();
+        assertTrue(news instanceof News);
+    }
+
+    @Test
+    public void getFunctionsWorkCorrectly(){
+        News news = simpleNews();
+        assertEquals(news.getTitle(),simpleNews().getTitle());
+        assertEquals(news.getDescription(),simpleNews().getDescription());
+        assertEquals(news.getType(),simpleNews().getType());
+    }
+
+    @Test
+    public void getDifferentNewsType(){
+        News news = simpleNews();
+        News news2 = altNews();
+        news2.setType("Current affairs");
+        news2.setAuthor("Star");
+        assertEquals(news.getType(),"General");
+        assertEquals(news2.getType(),"Current affairs");
+        assertEquals(news2.getAuthor(),"Star");
+    }
+
+
 }
