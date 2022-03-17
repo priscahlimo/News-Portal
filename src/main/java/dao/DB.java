c13be3b06744e8fb1f1012f6191488e1ff8df731
priscahlimo <priscahlimoh@gmail.com>
2022-03-17 12:00:08 +0300

    update postgress uri

--- src/main/java/dao/DB.java
+++ src/main/java/dao/DB.java
@@ -7,5 +7,5 @@ import org.sql2o.Sql2o;
 
 public class DB {
 
-    public static Sql2o sql2o = new Sql2o("postgres://gmpaixahqkhzxz:fef77d14054513a9f448c45eb65bdfc007c5074721ed5955c83fecc047bd72b7@ec2-34-192-83-52.compute-1.amazonaws.com:5432/d4moohjoqsa8eg", "gmpaixahqkhzxz", "fef77d14054513a9f448c45eb65bdfc007c5074721ed5955c83fecc047bd72b7");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://gmpaixahqkhzxz:fef77d14054513a9f448c45eb65bdfc007c5074721ed5955c83fecc047bd72b7@ec2-34-192-83-52.compute-1.amazonaws.com:5432/d4moohjoqsa8eg", "gmpaixahqkhzxz", "fef77d14054513a9f448c45eb65bdfc007c5074721ed5955c83fecc047bd72b7");
 }
